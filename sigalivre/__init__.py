b52d38afe72bfeb6fea4bf4887b524d5afc6ecb7
Andres MRM <andres@inventati.org>
2015-10-27 21:27:09 -0200

    mudanças para lidar com a nova interface do Siga

--- sigalivre/__init__.py
+++ sigalivre/__init__.py
@@ -18,6 +18,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # -----------------------------------------------------------------------------
 
+from __future__ import unicode_literals  # unicode by default
 
 import os
 import time
@@ -34,7 +35,7 @@ class SigaLivre(object):
         self.pasta = pasta
         self.navegador = None
         # Nome do arquivo baixado do sistema
-        self.arquivo = "Novo documento do Web Intelligence.csv"
+        self.arquivo = "null.csv"
 
     def criar_navegador(self):
         """Retorna um navegador firefox configurado para salvar arquivos
@@ -56,46 +57,102 @@ class SigaLivre(object):
         # driver = webdriver.PhantomJS(executable_path='phantomjs-download-support/bin/phantomjs')
         # driver = webdriver.Chrome()
 
-    def abrir_base(self, base):
+    def abrir_base(self, nome_base):
         """Navega no site do Senado até o 'lindo' sistema de consulta de dados.
         O nome passado é usado para escolher a base que será analisada"""
         print("Navegando...")
         print("    Site do Senado")
-        a ='http://www8a.senado.gov.br/dwweb/autoLogon.html'
+        a = 'http://www12.senado.gov.br/orcamento/sigabrasil'
         self.navegador.get(a)
         # time.sleep(5)
-        a = "http://www8a.senado.gov.br/AnalyticalReporting/WebiCreate.do?cafWebSesInit=true&appKind=InfoView&service=/InfoViewApp/common/appService.do&loc=pt&pvl=pt_BR&ctx=standalone&actId=223&containerId=3494812&pref=maxOpageU%3D100%3BmaxOpageUt%3D200%3BmaxOpageC%3D10%3Btz%3DAmerica%2FSao_Paulo%3BmUnit%3Dinch%3BshowFilters%3Dtrue%3BsmtpFrom%3Dtrue%3BpromptForUnsavedData%3Dtrue%3B"
-        self.navegador.get(a)
         print("    Escolhendo base e abrindo o 'lindo' sistema de consulta de dados")
-        self.navegador.find_element_by_partial_link_text(base).click()
+        a = 'http://www8d.senado.gov.br/dwweb/autoLogon.html'
+        self.navegador.get(a)
+        self.navegador.switch_to.frame('servletBridgeIframe')
+        self.navegador.find_element_by_partial_link_text(
+            'Página inicial').click()
+        self.navegador.switch_to.frame('iframeHome-76525')
+        self.navegador.find_element_by_id('id_50').click()
+        self.navegador.switch_to.parent_frame()
+        self.navegador.switch_to.frame(6)
+        self.navegador.switch_to.frame('webiViewFrame')
+        self.navegador.find_element_by_id('IconImg__dhtmlLib_188').click()
+        self.navegador.find_element_by_id('basicNaviTab_1_dsTypeBar').click()
+        self.navegador.find_element_by_id('OK_BTN_DataSourceTypeDlg').click()
+        bases = self.navegador.find_elements_by_class_name('mclM')
+        for base in bases:
+            if(base.text.find(nome_base) != -1):
+                base.click()
+                self.navegador.find_element_by_id('OK_BTN_addQueryDlg').click()
+                break
+        # self.navegador.find_element_by_partial_link_text(base).click()
 
     def escolher_dados(self, dados):
         """Adiciona os dados passados para serem consultados e executa a
         consulta"""
-        # time.sleep(5)
-        self.navegador.switch_to.frame("webiViewFrame")
-        self.navegador.switch_to.frame("querypanel")
+        # self.navegador.switch_to.frame("webiViewFrame")
+        # self.navegador.switch_to.frame("querypanel")
+
+        # id_input = 'outlineTree_0_searchWidget_text'
         print("    Escolhendo dados a serem exportados")
+        self.navegador.find_element_by_id(
+            "IconImg_outlineTree_0_expandAllIcon").click()
+        botoes = self.navegador.find_elements_by_class_name('treeNormal')
         for p in dados:
-            self.navegador.find_element_by_id("objectSearchZoneTxt").clear()
-            self.navegador.find_element_by_id("objectSearchZoneTxt").send_keys(p)
-            self.navegador.find_element_by_id("IconImg_iconMenu_icon_objectSearchZoneIcn").click()
-            self.navegador.find_element_by_id("theBttnIconaddObjButton").click()
-        self.navegador.switch_to.default_content()
-        self.navegador.switch_to.frame("webiViewFrame")
+            # self.navegador.find_element_by_id(id_input).clear()
+            # self.navegador.find_element_by_id(id_input).send_keys(p)
+            # self.navegador.find_element_by_id("IconImg_iconMenu_icon_objectSearchZoneIcn").click()
+            for botao in botoes:
+                if botao.text.strip() == p:
+                    botao.click()
+                    self.navegador.find_element_by_id(
+                        "RealBtn_addObjqpZone_0").click()
+                    break
+
+        # self.navegador.switch_to.default_content()
+        # self.navegador.switch_to.frame("webiViewFrame")
         # self.navegador.switch_to.parent_frame()
         print("    Rodando consulta")
-        self.navegador.find_element_by_id("IconImg_Txt_runquery").click()
+        self.navegador.find_element_by_id("IconImg_Txt_runQuery").click()
         time.sleep(20)
 
     def baixar_csv(self, pasta):
         """Inicia o download do CSV e aguarda ele terminar"""
-        self.navegador.find_element_by_id("IconImg_Txt_iconMenu_icon_docMenu").click()
+        # self.navegador.implicitly_wait(0)
+        # import IPython; IPython.embed()
+
+        # Costuma demorar muito para abrir o relatório...
+        self.navegador.implicitly_wait(200)
+
+        # Abrir menu de opções de dowloand
+        # botoes = self.navegador.find_elements_by_css_selector(
+        #     'div[id^="IconImg_iconMenu_icon__dhtmlLib_"]')
+        # estilo_botao_baixar_csv = "background-image:url('images/main/galleries/icon16x16gallery1b.png');background-position:0px -672px"
+        # for botao in botoes:
+        #     estilo = botao.get_attribute('style')
+        #     # Sim, vamos procurar o botão certo pelo estilo dele. Tenso...
+        #     if estilo.find(estilo_botao_baixar_csv) != -1:
+        #         botao.click()
+        #         break
+
+
+        # Escolher CSV
+        # botoes = self.navegador.find_elements_by_css_selector(
+        #     'div[id^="iconMenu_menu__dhtmlLib_"]')
+        # for botao in botoes:
+        #     if botao.text.find('Exportar dados para CSV...') != -1:
+        #         botao.click()
+        #         break
+
+        self.navegador.find_element_by_id(
+            "IconImg_iconMenu_icon__dhtmlLib_545").click()
+        self.navegador.implicitly_wait(20)
         time.sleep(2)
-        self.navegador.find_element_by_id("iconMenu_menu_docMenu_span_text_saveDocComputerAs").click()
+        self.navegador.find_element_by_id(
+            "iconMenu_menu__dhtmlLib_545_span_text__menuAutoId_54").click()
         time.sleep(2)
-        self.navegador.find_element_by_id("saveDocComputerMenu_text_saveCSV").click()
-        time.sleep(10)
+        self.navegador.find_element_by_id("BtnCImg_csvopOKButton").click()
+        time.sleep(20)
         # Enquanto ainda existir um arquivo temporário do firefox, esperar
         print("Baixando...")
         while self.arquivo + ".part" in os.listdir(pasta):
